0d0d2d382a52a1488ff028f45487c98d9c20f639
Mathias <mk@blackbird.online>
2023-11-09 11:17:46 +0100

    Simplify slightly

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,14 @@ mod _impl;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize, PartialEq)]
-pub struct SensorConfig {
-    pub p1: PinConfig,
-    pub p2: PinConfig,
-}
-
-#[derive(Clone, Default, PartialEq, Serialize, Deserialize, Debug)]
+#[derive(Default, PartialEq, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
 pub struct PinConfig {
     pub s1: PortMode,
     pub s2: PortMode,
 }
 
-#[derive(Clone, Default, PartialEq, Serialize, Debug)]
+#[derive(Default, PartialEq, Serialize, Debug)]
 #[serde(rename_all = "lowercase", tag = "mode", content = "config")]
 pub enum PortMode {
     #[default]
@@ -23,14 +17,14 @@ pub enum PortMode {
     Sio(SioConfig),
 }
 
-#[derive(Clone, Default, PartialEq, Serialize, Deserialize, Debug)]
+#[derive(Default, PartialEq, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
 pub struct SioConfig {
     pub mode: SioMode,
     pub debounce_us: u32,
 }
 
-#[derive(Clone, Copy, Default, PartialEq, Serialize, Deserialize, Debug)]
+#[derive(Default, PartialEq, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
 pub enum SioMode {
     Npn,
@@ -41,30 +35,23 @@ pub enum SioMode {
 #[cfg(test)]
 mod tests {
     use super::*;
+    const MAX_PAYLOAD_SIZE: usize = 512;
 
     #[test]
-    fn roundtrip_serialized_pin_config() {
-        let data = SensorConfig::default();
-
-        let serialized = serde_json_core::to_string::<_, 512>(&data).unwrap();
-
-        let (deserialized, _) = serde_json_core::from_str::<SensorConfig>(&serialized).unwrap();
-
+    fn roundtrip_json() {
+        let data = PinConfig::default();
+        let buf = &mut [0u8; MAX_PAYLOAD_SIZE];
+        let len = serde_json_core::to_slice(&data, buf).unwrap();
+        let (deserialized, _) = serde_json_core::from_slice::<PinConfig>(&buf[..len]).unwrap();
         assert_eq!(deserialized, data);
     }
 
     #[test]
-    fn persistance_roundtrip_postcard() {
-        const MAX_PAYLOAD_SIZE: usize = 512;
-
-        let data = SensorConfig::default();
-
+    fn roundtrip_postcard() {
+        let data = PinConfig::default();
         let buf = &mut [0u8; MAX_PAYLOAD_SIZE];
-
         let serialized = postcard::to_slice(&data, buf).unwrap();
-
-        let deserialized = postcard::from_bytes::<SensorConfig>(serialized).unwrap();
-
+        let deserialized = postcard::from_bytes::<PinConfig>(serialized).unwrap();
         assert_eq!(deserialized, data);
     }
 }
